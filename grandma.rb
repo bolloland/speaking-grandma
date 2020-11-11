d3bf33a9b152076888b043e5be354c0badb5a1e0
ryan.bollettino@gmail.com <bollo@bollo-laptop.localdomain>
2020-11-11 13:57:53 -0600

    commit tree a21406488ac6cf0eabeed6295d8650862c796a6b

--- grandma.rb
+++ grandma.rb
@@ -4,14 +4,14 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
-  else 
-    return "HUH?! SPEAK UP, SONNY!"
+    if phrase == "I LOVE YOU GRANDMA!"
+      return "I LOVE YOU TOO PUMPKIN!"
+    elsif phrase == phrase.upcase
+      return "NO, NOT SINCE 1938!"
+    else 
+      return "HUH?! SPEAK UP, SONNY!"
+    end
   end
-end
 
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
